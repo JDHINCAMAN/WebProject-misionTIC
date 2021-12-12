c7b209652ebf5f6750263acb7729c17833b74af7
Cristian <cstiven.osorioudea.edu.co>
2021-12-12 01:42:15 -0500

    se corrige inconveniente con index

--- Backend/index.js
+++ Backend/index.js
@@ -16,10 +16,7 @@ const server = new ApolloServer({
     const token = req.headers["authorization"] || "";
     if (token) {
       try {
-        const usuario = jwt.verify(
-          token.replace("Bearer ", ""),
-          process.env.JWT_SECRET
-        );
+        const usuario = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
         console.log(usuario);
         return {
           usuario,
@@ -33,12 +30,6 @@ const server = new ApolloServer({
 });
 
 // arrancar el servidor
-<<<<<<< HEAD
 server.listen({port: 4000,}).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
   });
-=======
-server.listen({ port: 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
->>>>>>> 11eb13347c4949dd623eedd725ee37c3fdbd8bd6
