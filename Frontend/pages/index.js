fc9de4d160df35f80ad5c75f1ec0d16199131869
crixtiam <cristiam9211@gmail.com>
2021-12-18 17:11:52 -0500

    dockerfiles

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import { useQuery, gql } from "@apollo/client";
 import Avances from "../components/Avances";
+//import React, { useState } from "react";
 
 const OBTENER_USUARIO = gql`
   query ObtenerUsuario {
