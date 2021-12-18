3041adba57eb139fa0cc60ec4fb8434cf95c4e59
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-12-18 00:46:40 -0500

    filtrado de proyectos again, mejoras

--- Frontend/components/NavProyectos.js
+++ Frontend/components/NavProyectos.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NavProyectos = () => {
+const NavProyectos = ({filter, setFilter}) => {
   return (
     <div className="px-6 pt-4">
       <ul className="flex">
@@ -8,19 +8,19 @@ const NavProyectos = () => {
           <a
             href="#"
             className="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-yellow-300 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:bg-gray-800"
+            onClick={() => setFilter("todos")}
           >
             Todos Los Proyectos
           </a>
-          <a href="#" className="ml-2 text-gray-500 hover:text-gray-900"></a>
         </li>
         <li className="text-gray-500 hover:text-white focus-within:text-white">
           <a
             href="#"
             className="inline-block w-full py-2 pl-8 pr-4 text-xs rounded hover:bg-yellow-300 focus:outline-none focus:ring-1 focus:ring-gray-500 focus:bg-gray-800"
+            onClick={() => setFilter("mis proyectos")}
           >
             Mis proyectos
           </a>
-          <a href="#" className="ml-2 text-gray-500 hover:text-gray-900"></a>
         </li>
       </ul>
     </div>
--- Frontend/components/Proyectos.js
+++ Frontend/components/Proyectos.js
@@ -5,6 +5,7 @@ import FormCrearProyectos from "./FormCrearProyectos";
 import { toast } from "react-toastify";
 import StepperProgress from "./StepperProgress";
 import NavProyectos from "./NavProyectos";
+import Proyecto from "./Proyecto";
 
 const OBTENER_PROYECTOS = gql`
   query ObtenerProyectos {
@@ -109,82 +110,28 @@ const Proyectos = ({ usuario }) => {
 
       {modal && <FormCrearProyectos handleClose={() => setModal(false)} />}
       <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 py-5">
-        {data.obtenerProyectos.map((proyect) => (
-          <div
-            className={`flex flex-col gap-x-5 bg-gray-300 mx-3 my-3 hover:bg-gray-100 w-4/5 h-auto items-center px-5 rounded-xl border-l-8 ${
-              proyect.estadoProyecto ? "border-l-green-600" : "border-l-red-600"
-            } relative`}
-          >
-            <button className="flex absolute z-10 right-2.5 top-2.5 ">
-              <svg
-                className="ml-8 h-6 w-6 text-gray-500 hover:text-gray-700"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-pencil-square"
-                viewBox="0 0 16 16"
-              >
-                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                <path
-                  fill-rule="evenodd"
-                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-                />
-              </svg>
-            </button>
+        {filter === "todos" &&
+          data.obtenerProyectos.map((proyect) => (
+            <Proyecto
+              key={proyect.id}
+              proyect={proyect}
+              functionClick={(e) => functionClick(e)}
+              usuario={usuario}
+              handleActualizarEstado={handleActualizarEstado}
+            />
+          ))}
 
-            <div className="flex items-center mt-3">
-              <h1 className="">{proyect.nombreProyecto}</h1>
-            </div>
-
-            <div className="px-1 pt-2">
-              <hr className="border border-gray-400 w-40" />
-            </div>
-            <h2>{proyect.objetivoGeneral}</h2>
-
-            <div className="flex w-full my-3">
-              <p>
-                Estado Actual:{" "}
-                <span
-                  className={`px-2  inline-flex text- leading-5 font-semibold rounded-full  ${
-                    proyect.estadoProyecto ? "bg-green-600" : "bg-red-600"
-                  } text-white`}
-                >
-                  {proyect.estadoProyecto ? "Activo" : "Inactivo"}
-                </span>
-              </p>
-            </div>
-
-            <div className="flex my-8 justify-around w-full">
-              {usuario.rol === "ADMINISTRADOR" && (
-                <button
-                  id={proyect.id}
-                  type="button"
-                  value={proyect.estadoProyecto}
-                  // className="bg-black w-20 h-9 text-sm shadow-sm font-medium tracking-wider border text-white rounded-full hover:shadow-lg hover:bg-gray-900"
-                  className={`${
-                    proyect.estadoProyecto ? "bg-red-600" : "bg-green-600"
-                  } w-20 h-9 text-sm shadow-sm font-medium tracking-wider border text-white rounded-full hover:shadow-lg hover:bg-gray-900`}
-                  onClick={handleActualizarEstado}
-                >
-                  {proyect.estadoProyecto ? "Descativar" : "Activar"}
-                </button>
-              )}
-
-              <button
-                id={proyect.id}
-                type="submit"
-                className="bg-black xl:w-2/5 text-sm shadow-sm font-medium tracking-wider border text-white rounded-full hover:shadow-lg hover:bg-gray-900"
-                onClick={(e) => functionClick(e)}
-              >
-                Detalles
-              </button>
-            </div>
-            <div className="my-3 flex w-full justify-center">
-              <StepperProgress faseProyecto={proyect.faseProyecto} />
-            </div>
-          </div>
-        ))}
+        {filter === "mis proyectos" &&
+          data.obtenerProyectos
+            .filter((proyect) => proyect.lider === usuario.id)
+            .map((proyect) => (
+              <Proyecto
+                key={proyect.id}
+                proyect={proyect}
+                functionClick={functionClick}
+                usuario={usuario}
+              />
+            ))}
         {showModal && (
           <MostrarProyecto
             handleClose={() => setShow(false)}
